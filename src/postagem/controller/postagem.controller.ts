7f5427362bd935b2d3622b46d087ddf104c37417
Michelle <michelle_g_silva@hotmail.com>
2025-12-02 18:19:45 -0300

    Alterado Classe PostagemController e PostagemService

--- src/postagem/controller/postagem.controller.ts
+++ src/postagem/controller/postagem.controller.ts
@@ -1,16 +1,50 @@
-import { Controller, Get, HttpCode, HttpStatus } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
 import { PostagemService } from "../services/postagem.service";
 import { Postagem } from "../entities/postagem.entity";
+import { ReturnDocument } from "typeorm";
 
 @Controller("/postagens") // Indica que a classe e uma controller
 export class PostagemController {
 
     constructor(private readonly postagemService: PostagemService) { }
 
-    @Get()
+    @Get() // Indica qual tipo de requesição esse métido é executado
     @HttpCode(HttpStatus.OK) // Monta a resposta HTTP para o Front com o status 200
     findAll(): Promise<Postagem[]> {
         return this.postagemService.findAll();
 
     }
+
+    @Get("/:id_post") // id = "1" => id = 1
+    @HttpCode(HttpStatus.OK)
+    findById(@Param('id_post', ParseIntPipe) id_post: number): Promise<Postagem> {
+        return this.postagemService.findById(id_post);
+
+    }
+    
+    @Get('/titulo/:titulo')
+    @HttpCode(HttpStatus.OK) // resposta padrão será o código de status HTTP OK → 200
+    findAllByTitulo(@Param('titulo') titulo: string): Promise<Postagem[]> {
+        return this.postagemService.findAllByTitulo(titulo);
+
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED) // resposta padrão será o código de status HTTP create
+    create(@Body() postagem: Postagem): Promise<Postagem> {
+      return this.postagemService.create(postagem);
+    }
+
+    @Put() // Mapeia todas as requisições do HTTP PUT
+    @HttpCode(HttpStatus.OK)
+    update(@Body() postagem: Postagem): Promise<Postagem> {
+        return this.postagemService.update(postagem); // executa e retorna atualizando o DB
+     }
+
+    @Delete('/:id')
+    @HttpCode(HttpStatus.NO_CONTENT) // resposta padrão o HTTP Status NO_CONTENT 🡪 204
+    delete(@Param('id', ParseIntPipe) id: number) {
+        return this.postagemService.delete(id); // retorna confirmação da exclusão do Objeto da Classe Postagem
+    }
+    
 }
--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from "@nestjs/common";
-import { Repository } from "typeorm";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { DeleteResult, ILike, Repository } from "typeorm";
 import { Postagem } from "../entities/postagem.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 
@@ -16,6 +16,42 @@ export class PostagemService {
 
         return await this.postagemRepository.find()
         
+    }
+
+    async findById(id: number): Promise<Postagem> {
+        const postagem = await this.postagemRepository.findOne({
+            where: { id }
+        })
+
+        if(!postagem){
+            throw new HttpException('Postagem não encontrada', HttpStatus.NOT_FOUND)
+        }
+
+        return postagem
+
+    }
+
+    async findAllByTitulo(titulo: string): Promise<Postagem[]> {
+        return await this.postagemRepository.find({
+            where: {
+                titulo: ILike(`%${titulo}%`) //  para não diferenciar letras maiusculas e minusculas
+            }
+        })
+
+        }
+
+    async create(postagem: Postagem): Promise<Postagem> { // Create recebe um objeto da classe Postagem e persiste os dados no DB
+        return await this.postagemRepository.save(postagem); // Resultado do save é um objeto da classe Postagem pesistido (salvo) no DB
+    }
+ 
+    async update(postagem: Postagem): Promise<Postagem> { // Update atualiza o DB da aplicação
+        await this.findById(postagem.id) // findByID verifica se a postagem existe
+         return await this.postagemRepository.save(postagem); // Save salva as alterações no DB na tb_postagens
+    }
+
+    async delete(id: number): Promise<DeleteResult> { // DeleteResult confirmará se o Objeto identificado pelo Atributo id foi apagado
+        await this.findById(id) // verifica se a postagem que será deletada existe
+        return await this.postagemRepository.delete(id); // Indica se a exclusão do Objeto foi bem sucedida
     }
 
 }
