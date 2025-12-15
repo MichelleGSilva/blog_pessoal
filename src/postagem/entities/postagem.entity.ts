f6bb6f0fadc5385f12092b8ab12043a288f8147a
Michelle <michelle_g_silva@hotmail.com>
2025-12-15 12:59:21 -0300

    Deploy do Projeto Blog Pessoal

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -2,30 +2,37 @@ import { IsNotEmpty } from "class-validator"
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm"
 import { Tema } from "../../tema/entities/tema.entity"
 import { Usuario } from "../../usuario/entities/usuario.entity"
+import { ApiProperty } from "@nestjs/swagger"
 
 @Entity({name: "tb_postagens"})
 export class Postagem {
 
-    @PrimaryGeneratedColumn()    
+    @PrimaryGeneratedColumn()   
+    @ApiProperty()   
     id: number
 
     @IsNotEmpty()
     @Column({length: 100, nullable: false})
+    @ApiProperty()  
     titulo: string
 
     @IsNotEmpty()
     @Column({length: 1000, nullable: false})
+    @ApiProperty()  
     texto: string
 
     @UpdateDateColumn()
+    @ApiProperty()  
     data: Date
     
+    @ApiProperty({ type: () => Tema })   
     @ManyToOne(() => Tema, (tema) => tema.postagem, { // Define um relacionamento MUITOS para UM (ManyToOne) 
         onDelete: "CASCADE" // Garante que ao deletar um Tema, todas as Postagens associadas a ele sejam removidas automaticamente
     })
     tema: Tema // → esse campo indica qual Tema a Postagem pertence | Cria o atributo "tema" na entidade atual onde sera criada uma chave estrangeira (FK) no banco de dados
 
         // Indica o lado MUITO do relacionamento, indicando que esse campo se conecta ao campo Postagem da Model Usuario
+    @ApiProperty({ type: () => Usuario })  
     @ManyToOne(() => Usuario, (usuario) => usuario.postagem, {
         onDelete: "CASCADE"
     })
--- src/tema/entities/tema.entity.ts
+++ src/tema/entities/tema.entity.ts
@@ -1,17 +1,21 @@
 import { IsNotEmpty } from "class-validator";
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { Postagem } from "../../postagem/entities/postagem.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 @Entity({name: "tb_temas"})
 export class Tema {
 
-    @PrimaryGeneratedColumn()    
+    @PrimaryGeneratedColumn() 
+    @ApiProperty()    
     id: number
 
     @IsNotEmpty() // quando cadastrar a postagem no insomnia o campo não pode estar vazio
     @Column({length: 255, nullable: false}) // construção da tabela
+    @ApiProperty() 
     descricao: string
 
+    @ApiProperty() 
     @OneToMany(() => Postagem, (postagem) => postagem.tema) 
     postagem: Postagem[]
     /* Um tema pode ter várias postagens vinculadas | Uma postagem possui apenas um tema
